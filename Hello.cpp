bf3d18819c864eaabe3f94ef2f450d2339d3987d
neo <bbesecke@mavs.coloradomesa.edu>
2024-01-25 09:48:20 -0700

    V2.2

--- Hello.cpp
+++ Hello.cpp
@@ -11,10 +11,11 @@ First Modified Hello World
 
  int main ()
  {
-    // This will produce a modified quote from War Games 
-    // With Matthew Brodrick
+    /* This will produce a modified quote from War Games 
+     with Matthew Brodrick
+     */
     cout << "Hello Professor Bergen. Would you like to play a game?" << endl;
-    cout << " I like Global Thermonuclear War" << endl;
+    cout << "I like Global Thermonuclear War" << endl;
     
 
     return 0; // This exits the program
