fffa28f47a16275cad53f39106ec71d0331ee1a9
neo <bbesecke@mavs.coloradomesa.edu>
2024-01-30 09:50:47 -0700

    v1

---
+++ variable.cpp
@@ -0,0 +1,13 @@
+/* Burton Besecke all about the variables*/
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int number1 = 42;
+    cout << number1 << endl;
+    
+    return 0;
+}
