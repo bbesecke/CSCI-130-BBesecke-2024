a153d32bfeb537771a9cb144660cccad80c1387c
neo <bbesecke@mavs.coloradomesa.edu>
2024-01-31 09:20:31 -0700

    v2

--- variable.cpp
+++ variable.cpp
@@ -6,10 +6,14 @@ using namespace std;
 
 int main ()
 {
-    int number1;
-    int number2;
-    cout << &number1 << endl;
-    cout << &number2 << endl;
+    // int number1;
+    // int number2;
+    // cout << &number1 << endl;
+    // cout << &number2 << endl;
     
+    int number1;
+     number1 = 42;
+     cout << number1 << endl;
+     
     return 0;
 }
