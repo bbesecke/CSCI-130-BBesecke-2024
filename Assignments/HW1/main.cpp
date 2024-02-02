88ae536bc53dd191c270728b9ced6b4c41607f81
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-02 09:40:34 -0700

    V1 02 Feb

---
+++ Assignments/HW1/main.cpp
@@ -0,0 +1,23 @@
+/*Burton Besecke csci 130 HW1
+Stages of Hangman Game
+02 Feb 2024*/
+
+{
+
+#include <iostream>
+
+ using namespace std;
+
+ int main ()
+string Player1;
+
+ cout << " Player 1 ";
+cin >> Player1;
+
+string Player2;
+
+cout << " Player 2 ";
+cin >> Player2;
+
+
+}
