41ba20c20b37d92dfdab879f8c6c39af1a411053
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-02 08:59:37 -0700

    02 Feb

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # CSCI-130-BBesecke-2024
-
-This is a new line
-At least I am only 75% confused at this point
-
-This is not a new line....
-
-This is the file that never ends. Yes, it goes on, and on, and on my friend. Someone started making it not knowing what it was, now that person will keep making it because.....
+Set up Lab and HW folders under Assignments
--- variable.cpp
+++ variable.cpp
@@ -11,9 +11,35 @@ int main ()
     // cout << &number1 << endl;
     // cout << &number2 << endl;
     
-    int number1;
-     number1 = 42;
-     cout << number1 << endl;
-     
-    return 0;
+    // int number1;
+    //  number1 = 42;
+    //  cout << number1 << endl;
+
+    // string; charArg1;
+
+
+    // cout << " String is ";
+    // cout << sizeof(string);
+    // cout << " bytes"; << endl;
+
+//  string name1;
+
+//  cout << "My name is ";
+
+//  getline(cin, name1)
+  
+// cin >> name1;
+
+// cout << "Welcom to my world  " << name1
+//      << ", prepare to expierence my pain!" << endl;
+// return 0;
+
+string somePhrase;
+
+somePhrase = "This is a \"phrase\" that I'm typing.\n"
+"Why not have another \"phrase\".";
+
+  cout << somePhrase << endl;
+return 0;
+
 }
