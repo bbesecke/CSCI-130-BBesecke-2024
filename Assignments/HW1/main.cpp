005bbc7e60db806b96e6d258484d343420aa6d9f
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-07 08:19:15 -0700

    07 Feb 0819

--- Assignments/HW1/main.cpp
+++ Assignments/HW1/main.cpp
@@ -16,34 +16,31 @@ Started 02 Feb 2024*/
 
  cin >> playerOne;
 
- cout << "Welcome " << playerOne << ". Gird your loins for battle and rejoice,\n"
-  "for the chance to prove your metal!";
-
  string playerTwo;
 
  cout << "Who is player 2?";
 
  cin >> playerTwo;
 
- cout << "Welcome " << playerTwo << ". Fear neither defeat or success,\n"
- " for they are just different sides of the coin of life!";
+ cout << "Welcome " << playerOne << "&" << playerTwo << ". Fear neither defeat or success,\n"
+ " for they are just different sides of the same coin of life!";
 
- string stage1 = "After entering player information, and game play is started, the scaffold in which \n";
+ string line1 = "After entering player information, and game play is started, the scaffold in which \n";
  "to build the body of the victim is built";
- cout << stage1;
- string stage2 = "The body is now starting to be constructed with pieces being added as players\n";
+ cout << line1;
+ string line2 = "The body is now starting to be constructed with pieces being added as players\n";
  "miss their guesses for letters";
- cout << stage2;
- string stage3 = "First the head appears";
- cout << stage3;
- string stage4 = "Then the body is added to the bottom of the head\n";
- cout << stage4;
- string stage5 = "The arms are then added to the sides of the body, one at a time\n";
- cout << stage5;
- string stage6 = "The legs come next one at a time to the bottom of the body\n";
- cout << stage6;
- string stage7 = "The game ends when the word is completed or the victims body is completed"; 
- cout << stage7 << endl;
+ cout << line2;
+ string line3 = "First the head appears";
+ cout << line3;
+ string line4 = "Then the body is added to the bottom of the head\n";
+ cout << line4;
+ string line5 = "The arms are then added to the sides of the body, one at a time\n";
+ cout << line5;
+ string line6 = "The legs come next one at a time to the bottom of the body\n";
+ cout << line6;
+ string line7 = "The game ends when the word is completed or the victims body is completed"; 
+ cout << line7 << endl;
 
 
  // wait for players to finish before exiting//
