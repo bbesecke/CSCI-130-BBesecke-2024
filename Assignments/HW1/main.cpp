dbe8f201b174d1109fb62355aecc4872802de4e3
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-06 18:59:58 -0700

    o6 feb 1900

--- Assignments/HW1/main.cpp
+++ Assignments/HW1/main.cpp
@@ -11,7 +11,7 @@ Started 02 Feb 2024*/
 {
     // string myName;
 
-    // cout << " Same my name";
+    // cout << " Say my name";
 
     // cin >> myName;
 
@@ -24,7 +24,8 @@ string playerOne;
 
  cin >> playerOne;
 
- cout << "Welcome " << playerOne << ". Gird your loins for battle and rejoice for the chance to prove your metal!" << endl;
+ cout << "Welcome " << playerOne << ". Gird your loins for battle and rejoice,\n"
+  "for the chance to prove your metal!";
 
 string playerTwo;
 
@@ -32,16 +33,22 @@ cout << "Who is player 2?";
 
 cin >> playerTwo;
 
-cout << "Welcome " << playerTwo << ". Fear neither defeat or success for they are just different sides of the coin of life!" << endl;
+cout << "Welcome " << playerTwo << ". Fear neither defeat or success,\n"
+" for they are just different sides of the coin of life!";
 
-string stage1 =  "After entering player information, and game play is started, the scaffold in which \n";
+string stage1 = "After entering player information, and game play is started, the scaffold in which \n";
 "to build the body of the victim is built";
-cout << stage1;
+cout << stage1 << endl;
+
 string stage2 = "The body is now starting to be constructed with pieces being added as players\n"
 "miss thier guesses for letters";
+cout << stage2 << endl;
+
 
 
 // wait for players to finish before exiting//
-getchar();
+
+system ("PAUSE");
 return 0;
+
 }
