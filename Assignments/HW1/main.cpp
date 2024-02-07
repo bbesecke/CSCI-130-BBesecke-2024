1759989883797e0a0e4f475f4ef6d94c51b1fb49
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-07 09:30:58 -0700

    Feb 07 0930

--- Assignments/HW1/main.cpp
+++ Assignments/HW1/main.cpp
@@ -22,8 +22,9 @@ Started 02 Feb 2024*/
 
  cin >> playerTwo;
 
- cout << "Welcome " << playerOne << " & " << playerTwo << " These are the stages of Handgman.\n";
-
+ cout << "Welcome " << playerOne << " & " << playerTwo << ". These are the 7 stages of Handgman.\n"
+ "Yes, the stages start at 0\n";
+ 
  cout << "Stage 0:\n" 
          "   |----------------\n";
  cout << "   |/          |\n";
@@ -34,6 +35,67 @@ Started 02 Feb 2024*/
  cout << "   |\n";
  cout << "====================\n";
  
+ cout << "Stage 1:\n" 
+         "   |----------------\n";
+ cout << "   |/          |\n";
+ cout << "   |           O\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "====================\n";
+
+ cout << "Stage 2:\n" 
+         "   |----------------\n";
+ cout << "   |/          |\n";
+ cout << "   |           O\n";
+ cout << "   |           |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "====================\n";
+
+ cout << "Stage 3:\n" 
+         "   |----------------\n";
+ cout << "   |/          |\n";
+ cout << "   |           O\n";
+ cout << "   |          /|\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "====================\n";
+
+ cout << "Stage 4:\n" 
+         "   |----------------\n";
+ cout << "   |/          |\n";
+ cout << "   |           O\n";
+ cout << "   |          /|\\\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "====================\n";
+
+ cout << "Stage 5:\n" 
+         "   |----------------\n";
+ cout << "   |/          |\n";
+ cout << "   |           O\n";
+ cout << "   |          /|\\\n";
+ cout << "   |          /\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "====================\n";
+
+cout << "Stage 6:\n" 
+         "   |----------------\n";
+ cout << "   |/          |\n";
+ cout << "   |           O\n";
+ cout << "   |          /|\\\n";
+ cout << "   |          / \\\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "====================\n";
+
+ cout << "It was later discovered the poor person was innocent and exonerated of all crimes on record";
 
  // wait for players to finish before exiting//
 
