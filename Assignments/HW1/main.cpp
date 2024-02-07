b43d87fc1d8591ca4b06a55f3a33b85a21c22f37
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-06 19:17:04 -0700

    06 Feb 1916

--- Assignments/HW1/main.cpp
+++ Assignments/HW1/main.cpp
@@ -2,23 +2,15 @@
 Stages of Hangman Game
 Started 02 Feb 2024*/
 
-#include <iostream>
+ #include <iostream>
 
  using namespace std;
 
  int main()
 
-{
-    // string myName;
+ {
 
-    // cout << " Say my name";
-
-    // cin >> myName;
-
-    // cout << " Your name is " << myName << " master!" << endl;
-
-
-string playerOne;
+ string playerOne;
 
  cout << "Who is Player 1?";
 
@@ -27,28 +19,35 @@ string playerOne;
  cout << "Welcome " << playerOne << ". Gird your loins for battle and rejoice,\n"
   "for the chance to prove your metal!";
 
-string playerTwo;
+ string playerTwo;
 
-cout << "Who is player 2?";
+ cout << "Who is player 2?";
 
-cin >> playerTwo;
+ cin >> playerTwo;
 
-cout << "Welcome " << playerTwo << ". Fear neither defeat or success,\n"
-" for they are just different sides of the coin of life!";
+ cout << "Welcome " << playerTwo << ". Fear neither defeat or success,\n"
+ " for they are just different sides of the coin of life!\n\n";
 
-string stage1 = "After entering player information, and game play is started, the scaffold in which \n";
-"to build the body of the victim is built";
-cout << stage1 << endl;
+ string stage1 = "After entering player information, and game play is started, the scaffold in which \n";
+ "to build the body of the victim is built";
+ cout << stage1;
+ string stage2 = "The body is now starting to be constructed with pieces being added as players\n";
+ "miss their guesses for letters";
+ cout << stage2;
+ string stage3 = "First the head appears";
+ cout << stage3;
+ string stage4 = "Then the body is added to the bottom of the head\n";
+ cout << stage4;
+ string stage5 = "The arms are then added to the sides of the body, one at a time\n";
+ cout << stage5;
+ string stage6 = "The legs come next one at a time to the bottom of the body\n";
+ cout << stage6;
+ string stage7 = "The game ends when the word is completed or the victims body is completed"; 
 
-string stage2 = "The body is now starting to be constructed with pieces being added as players\n"
-"miss thier guesses for letters";
-cout << stage2 << endl;
 
+ // wait for players to finish before exiting//
 
+ getchar();
+ return 0;
 
-// wait for players to finish before exiting//
-
-system ("PAUSE");
-return 0;
-
-}
+ }
