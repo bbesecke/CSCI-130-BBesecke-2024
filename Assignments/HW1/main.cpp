92c7e74108cd91fb1c34cb624049c26029fcb98b
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-07 08:34:50 -0700

    07 Feb 0834

--- Assignments/HW1/main.cpp
+++ Assignments/HW1/main.cpp
@@ -23,23 +23,23 @@ Started 02 Feb 2024*/
  cin >> playerTwo;
 
  cout << "Welcome " << playerOne << "&" << playerTwo << ". Fear neither defeat or success,\n"
- " for they are just different sides of the same coin of life!";
+ " for they are just different sides of the same coin of life!\n";
 
- string line1 = "After entering player information, and game play is started, the scaffold in which \n";
+ string line1 = " First stage after entering player information, and game play is started, the scaffold in which \n";
  "to build the body of the victim is built";
  cout << line1;
- string line2 = "The body is now starting to be constructed with pieces being added as players\n";
+ string line2 = "Stage 2: The body is now starting to be constructed with pieces being added as players\n";
  "miss their guesses for letters";
  cout << line2;
- string line3 = "First the head appears";
+ string line3 = "Stage 3: The head appears with the 1st missed letter";
  cout << line3;
- string line4 = "Then the body is added to the bottom of the head\n";
+ string line4 = "Stage 4: Then after the second missed letter, the body is added to the bottom of the head\n";
  cout << line4;
- string line5 = "The arms are then added to the sides of the body, one at a time\n";
+ string line5 = "stage 5: The arms are then added to the sides of the body, one at a time\n";
  cout << line5;
- string line6 = "The legs come next one at a time to the bottom of the body\n";
+ string line6 = "Stage 6: The legs come next one at a time to the bottom of the body\n";
  cout << line6;
- string line7 = "The game ends when the word is completed or the victims body is completed"; 
+ string line7 = "Stage 7: The game ends when the word is completed or the victims body is completed"; 
  cout << line7 << endl;
 
 
