47aa261d55b87321f13aa71d2ed54969a41cfc62
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-07 09:18:18 -0700

    Feb 07 No string var

--- Assignments/HW1/main.cpp
+++ Assignments/HW1/main.cpp
@@ -24,24 +24,16 @@ Started 02 Feb 2024*/
 
  cout << "Welcome " << playerOne << " & " << playerTwo << " These are the stages of Handgman.\n";
 
- string line1 = "Stage 0:\n"
-                 "  |----------------\n";
- cout << line1;
- string line2 =  "  |/           |\n";
- cout << line2;
- string line3 =  "  |\n";
- cout << line3;
- string line4 =  "  |\n";
- cout << line4;
- string line5 =  "  |\n";
- cout << line5;
- string line6 =  "  |\n";
- cout << line6;
- string line7 =  "  |\n";
- cout << line7;
- string line8 =  "====================\n";
- cout << line8;
-
+ cout << "Stage 0:\n" 
+ "           |----------------\n";
+ cout <<"    |/           |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "   |\n";
+ cout << "====================\n";
+ 
 
  // wait for players to finish before exiting//
 
