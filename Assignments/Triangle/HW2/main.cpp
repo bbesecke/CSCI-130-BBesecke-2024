5d56a94aba6b6efe109c9b231c3b5e03103a4745
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-10 15:04:28 -0700

    10 Feb 15105

---
+++ Assignments/Triangle/HW2/main.cpp
@@ -0,0 +1,5 @@
+// Burton Besecke CSCI 130-002
+/*Calculating the area and perimiter of a triangle 
+with user input of the length of the 3 sides. 
+Herons formula for area and adding length of sides. */
+
