a0949473d45a004e93e033c4ce0b8b01117d0108
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-10 15:18:34 -0700

    10 Feb 1530

--- Assignments/Triangle/HW2/main.cpp
+++ Assignments/Triangle/HW2/main.cpp
@@ -3,3 +3,40 @@
 with user input of the length of the 3 sides. 
 Herons formula for area and adding length of sides. */
 
+#include <iostream>
+
+ using namespace std;
+
+ int main ()
+ {
+    // setting up variables and prompting user input for side lengths of triangle
+
+    float sideOne, sideTwo, sideThree;
+
+    cout << "To determine the area and permiter of your triangle,\n"
+    "please enter the length of the sides.\n";
+    "\n"
+    "Length of side 1?";
+
+    cin >> sideOne;
+
+    cout << "The first side is "<< sideOne <<".\n"
+    "Enter the length of the second side.";
+
+    cin >> sideTwo;
+
+      cout << "The second side is "<< sideTwo <<".\n"
+    "Enter the length of the last side.";
+
+    cin >> sideThree;
+
+    cout << "Your triangle sides are," << sideOne << "," << sideTwo << ",and" << sideThree;
+
+
+
+
+    getchar();
+    return 0;
+
+
+ }
