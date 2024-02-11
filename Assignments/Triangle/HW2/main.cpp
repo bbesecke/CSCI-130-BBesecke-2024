aee51b8a7cc32605d69a8467756fa1b41940817b
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-11 10:48:46 -0700

    11 Feb 1048

--- Assignments/Triangle/HW2/main.cpp
+++ Assignments/Triangle/HW2/main.cpp
@@ -1,9 +1,11 @@
 // Burton Besecke CSCI 130-002
-/*Calculating the area and perimiter of a triangle 
+/*Calculating the area using Herons formula and the perimiter of a triangle  
 with user input of the length of the 3 sides. 
-Herons formula for area and adding length of sides. */
+Herons formula ((s= sideOne + sideTwo + sideThree / 2) 
+then sqrt(s * (s - sideOne) * (s - sideTwo) * (s - sideThree)*/
 
-#include <iostream>
+ #include <iostream>
+ #include <cmath>
 
  using namespace std;
 
@@ -13,6 +15,7 @@ Herons formula for area and adding length of sides. */
 
     float sideOne, sideTwo, sideThree;
 
+    // Describing to user what is being calculated and entering side variables
     cout << "To determine the area and permiter of your triangle,\n"
     "please enter the length of the sides.\n"
     "\n"
@@ -30,17 +33,38 @@ Herons formula for area and adding length of sides. */
 
       cout << "The second side is "<< sideTwo <<".\n"
      "\n"
-    "Enter the length of the last side: ";
+    "Enter the length of the third side: ";
 
     cin >> sideThree;
 
     cout << "\n";
 
+    //Telling user what values they entered in case of improper input
     cout << "Your triangle sides are, " << sideOne << ", " << sideTwo << ",and " << sideThree;
+
     cout << "\n";
 
+    float sum1;
 
+    sum1 = sideOne + sideTwo + sideThree;
+    
+    // Summing the 3 sides to give perimeter length
+    cout << "The triangles perimeter length is " << sum1;
 
+    //Setting up Herons formula by setting variable sum2 = s in terms of sideOne, sideTwo and sideThree. 
+    float sum2;
+    
+    sum2 = (sum1/2);
+    
+    float sum3;
+
+    sum3 = sqrt(sum2 * (sum2 - sideOne) * (sum2 - sideTwo) * (sum2 - sideThree));
+
+    cout << "\n";
+
+    cout << "The triangles area is " << sum2;
+
+    cout << "\n";
 
     getchar();
     return 0;
