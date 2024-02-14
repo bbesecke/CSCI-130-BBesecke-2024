60f6ffee987173dc8895fe034648b619082a96f7
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-14 09:24:35 -0700

    Feb 14 0930

---
+++ Assignments/Triangle/HW2/rectangleCalc.cpp
@@ -0,0 +1,38 @@
+ //BBesecke 
+// Rectangle calculator 
+ //Just because
+ //Greet user with name prompt
+
+ #include <iostream>
+ 
+ using namespace std;
+ float main()
+   {
+    //Display name prompt
+     string promptname()
+   }
+ {
+    string inputName;
+
+    cout << "Please enter your name";
+    getline(cin, inputName);
+    return inputName;
+ }
+
+ //Display greeting
+ void greetUser(string name)
+ {
+    cout << "welcome " << name << "to the rectangle calculator";
+    << endl;
+ }
+
+ //Prompt for side 1 and then side 2 (float)
+ float getSides();
+
+ //Calc perimiter <perim = 2(side2 + side2)>
+ float calcPerim(float, float)
+
+ return 0.0;
+
+  
+   }
