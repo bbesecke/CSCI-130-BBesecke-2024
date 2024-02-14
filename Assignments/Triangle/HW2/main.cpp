fe8a20ac335e19fff06a58a54224279892635993
neo <bbesecke@mavs.coloradomesa.edu>
2024-02-14 09:50:07 -0700

    Feb 14 0950

--- Assignments/Triangle/HW2/main.cpp
+++ Assignments/Triangle/HW2/main.cpp
@@ -10,7 +10,7 @@ then sqrt(s * (s - sideOne) * (s - sideTwo) * (s - sideThree)*/
 
  using namespace std;
 
- float main ()
+    int main ()
  {
     // setting up variables and prompting user input for side lengths of triangle
 
@@ -18,46 +18,57 @@ then sqrt(s * (s - sideOne) * (s - sideTwo) * (s - sideThree)*/
 
     // Describing to user what is being calculated and entering side variables
     cout << "To determine the area and permiter of your triangle,\n"
-    "please enter the length of the sides.\n"
-    "\n"
-    "What is the length of side 1: ";
+    "please enter the length of the sides.\n";
+
+    cout << "\n";
+
+    cout << "What is the length of side 1: ";
 
     cin >> sideOne;
 
     cout << "\n";
 
-    cout << "The first side is "<< sideOne <<".\n"
-     "\n"
+    cout << "The first side is ";
+    cout << fixed << setprecision(2) << sideOne;
+    cout << "\n"
     "Enter the length of the second side: ";
 
     cin >> sideTwo;
 
     cout << "\n";
 
-    cout << "The second side is "<< sideTwo <<".\n"
-     "\n"
-    "Enter the length of the third side: ";
+    cout << "The second side is ";
+    cout << fixed << setprecision(2) << sideTwo;
+
+    cout << "\n";
+
+    cout << "Enter the length of the third side: ";
 
     cin >> sideThree;
 
     cout << "\n";
 
-    cout << "The third side is " << sideThree;
+    cout << "The third side is ";
+    cout << fixed << setprecision(2) << sideThree;
 
     cout << "\n";
     cout << "\n";
 
     //Telling user what values they entered in case of improper input
     cout << "Your triangle sides are, 1: " << sideOne << ", 2: " << sideTwo << ", and 3: " << sideThree;
-
+     
     cout << "\n";
 
+ // Summing the 3 sides to give perimeter length
     float sum1;
 
     sum1 = sideOne + sideTwo + sideThree;
     
-    // Summing the 3 sides to give perimeter length
-    cout << "The triangles perimeter length is " << sum1;
+    cout << "The triangles perimeter length is ";
+    
+    cout << fixed << setprecision(2) << sum1;
+
+    cout << " units";
 
     //Setting up Herons formula by setting variable s = sum2 in terms of sideOne, sideTwo and sideThree. 
     float sum2;
@@ -70,9 +81,11 @@ then sqrt(s * (s - sideOne) * (s - sideTwo) * (s - sideThree)*/
 
     cout << "\n";
 
-    cout << setprecision (2);
+    cout << "The triangles area is ";
 
-    cout << "The triangles area is " << sum3;
+    cout << fixed << setprecision(2) << sum3;
+
+    cout << " units";
 
     cout << "\n";
 
