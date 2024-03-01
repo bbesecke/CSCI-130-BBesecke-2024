a5d7524b1268c38f63c85d91e32d96a5afb4c80e
neo <bbesecke@mavs.coloradomesa.edu>
2024-03-01 09:35:11 -0700

    01 Mar

--- Labs/L3/functions.cpp
+++ Labs/L3/functions.cpp
@@ -2,7 +2,7 @@
 Functions Lab
 Updated By: Burton Besecke
 CSCI 130-002
-Date: 23 Feb 2024 
+Date: 23 Feb 2024
 
 Program prompts the user to enter two points in the form (x1, y1) and (x2, y2) and finds the distance between the two points using a function.
 Algorithm steps:
@@ -36,59 +36,63 @@ void test();
 
 // function clears the screen system call
 // NOTE: system call is not a security best practice!
-void clearScreen() 
+void clearScreen()
 {
     // use "cls" in windows and "clear" command in Mac and Linux
-        system("clear");
-
+    system("clear");
 }
 
 int main()
 {
     // variables to store two points (x1, y1) and (x2, y2)
-    int x1, y1, x2, y2; 
+    int x1, y1, x2, y2;
     char ch;
 
-    //FIXME-bonus - 10 bonus points - add loop until user wants to quit
-    // the loop will execute the following block of code
+    // FIXME-bonus - 10 bonus points - add loop until user wants to quit
+    //  the loop will execute the following block of code
+
+    char repeat = 'y';
+
+    while (repeat != 'n')
     {
+
         clearScreen();
         cout << "Program calculates distance between 2 points on a 2D coordinate." << endl;
         cout << "Enter a point in the form (x, y): ";
-      
+
         // parse the input stream
-        cin >> ch >> x1 >> ch >> y1 >> ch; 
-        
+        cin >> ch >> x1 >> ch >> y1 >> ch;
+
         // value stored in ch is ignored
         printf("(x1, y1) = (%d, %d)\n", x1, y1);
 
         cout << "Enter a second point in the form (x, y): ";
         cin >> ch >> x2 >> ch >> y2 >> ch;
-       
+
         printf("(x2, y2) = (%d, %d)\n", x2, y2);
-        //FIXME3 - Read/parse the second point and store data into variables x2 and y2 FIXED
-       
-        //FIXME4 - Call test function FIXED
+        // FIXME3 - Read/parse the second point and store data into variables x2 and y2 FIXED
+
+        // FIXME4 - Call test function FIXED
         test();
 
-        //FIXME5 - call findDistance function passing proper arguments FIXED
-       findDistance(x1, y1, x2, y2);
-        //FIXME6 – Using printf function display the returned distance with proper description FIXED
-        printf("The distance between the 2 points is %f ", findDistance(x1, y1, x2, y2));
+        // FIXME5 - call findDistance function passing proper arguments FIXED
+        findDistance(x1, y1, x2, y2);
+        // FIXME6 – Using printf function display the returned distance with proper description FIXED
+        printf("The distance between the 2 points is %f \n", findDistance(x1, y1, x2, y2));
+
+        cout << "Do you want to calculate another distance? Enter y for yes, or n for no: ";
+        cin >> repeat;
     }
 
-    cin.ignore(1000, '\n');
-    cout << "Enter to quit the program: ";
-    cin.get();
-    cout << "Good bye..." << endl;
+
     return 0;
 }
 
 double findDistance(int x1, int y1, int x2, int y2)
 {
-    
+
     // FIXME7 - Find the distance between (x1, y1) and (x2, y2) FIXED
-    double result = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+    double result = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     // following the algorithm in step 1
     // return the calculated distance
     return result;
@@ -97,16 +101,16 @@ double findDistance(int x1, int y1, int x2, int y2)
 // test function that test findDistance function with 3 test cases
 void test()
 {
-    float result =  findDistance(4, 3, 5, 1);
+    float result = findDistance(4, 3, 5, 1);
     float expected = 2.236067f;
-    assert( fabs(result - expected) <= epsilon); 
-    //accept the result if it's less than the error of margin
-    // FIXME8 - add at least two more test cases FIXED
-    float result2 =  findDistance(1, 2, 3, 4);
+    assert(fabs(result - expected) <= epsilon);
+    // accept the result if it's less than the error of margin
+    //  FIXME8 - add at least two more test cases FIXED
+    float result2 = findDistance(1, 2, 3, 4);
     float expected2 = 2.828427f;
-    assert( fabs(result2 - expected2) <= epsilon); 
-    float result3 =  findDistance(3, 3, 4, 5);
+    assert(fabs(result2 - expected2) <= epsilon);
+    float result3 = findDistance(3, 3, 4, 5);
     float expected3 = 2.236068f;
-    assert( fabs(result3 - expected3) <= epsilon); 
+    assert(fabs(result3 - expected3) <= epsilon);
     cerr << "all tests passed..." << endl;
 }
