97cf07315a5a25a4e332b035728c309a930d6336
neo <bbesecke@mavs.coloradomesa.edu>
2024-03-04 08:32:48 -0700

    04 Mar

---
+++ Assignments/HW3/functions.cpp
@@ -0,0 +1,56 @@
+/* Name: Burton Besecke 
+Class: CSCI 130-002
+1)Define a function that takes two numbers, adds two numbers and returns the
+sum. The function must be templated so it works for integers and floats. (5
+points)
+
+2) Define a function that takes two numbers, multiplies the two numbers and returns
+the product. The function must be templated so it works for integers and floats. (5
+points)
+
+3) Define a function that takes two numbers; divides the first number by the second
+and returns the quotient. The function must be templated so it works for integers
+and floats. (5 points)
+
+4) Define a function that takes two numbers, subtracts the second from the first and
+returns the difference. The function must be templated so it works for integers
+and floats. (5 points)
+
+5) Define a function that takes two numbers, finds and returns the remainder of the
+first number divided by the second. The function must be templated so it works
+for integers and floats. (5 points)
+
+6) Define a function that takes two numbers, finds the first to the power of the
+second number and returns the result. The function must be templated so it
+works for integers and floats. (5 points)
+
+7) Define a function that takes a number and returns the square-root of the number.
+The function must be templated so it works for integers and floats. (5 points)
+8) Prompt the user to enter two numbers. (5 points)
+
+9) Call all the functions passing those two entered numbers and print the calculated
+results with proper descriptions. (10 points)
+
+10) Write a test function to automatically test each function with at least two different
+test cases. (5*7 = 35 points)
+
+*/
+ 
+ #include <iostream>
+
+ using namespace std;
+
+ int main ()
+
+ {
+    cout << " Yip Yip Haw" << endl;
+
+    cout << "\n";
+
+    cin.get();
+
+    getchar();
+
+    return 0;
+    
+ }
