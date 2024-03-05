0d7f02b15237be7d02fefd8517abee3c197d6b31
neo <bbesecke@mavs.coloradomesa.edu>
2024-03-05 15:26:09 -0700

    05 Mar

--- Assignments/HW3/functions.cpp
+++ Assignments/HW3/functions.cpp
@@ -2,14 +2,6 @@
 Class: CSCI 130-002
 
 
-
-2) Define a function that takes two numbers, multiplies the two numbers and returns
-the product. The function must be templated so it works for integers and floats.
-
-3) Define a function that takes two numbers; divides the first number by the second
-and returns the quotient. The function must be templated so it works for integers
-and floats.
-
 5) Define a function that takes two numbers, finds and returns the remainder of the
 first number divided by the second. The function must be templated so it works
 for integers and floats.
@@ -49,16 +41,31 @@ float subtract(float num1, float num2)
 {
     return num2 - num1;
 }
+/*Define a function that takes two numbers, multiplies the two numbers and returns the product.*/
+float multiply(float num1, float num2)
 
+{
+    return num1 * num2;
+}
+
+/*Define a function that takes two numbers; divides the first number by the second and returns the quotient.*/
+
+float divide(float num1, float num2)
+
+{
+    return num1 / num2;
+}
+
+// Clears screen when program is run//
 void clearScreen()
 {
-    // use "cls" in windows and "clear" command in Mac and Linux
+    // use "cls" in windows and "clear" command in Mac and Linux//
     system("clear");
 }
 
 int main()
 {
-
+    // Loop for main//
     char repeat = 'y';
 
     while (repeat != 'n')
@@ -67,16 +74,32 @@ int main()
         clearScreen();
         float num1, num2, sum;
 
-        /* Prompt the user to enter two numbers.*/
+        // Prompt the user to enter two numbers.//
         cout << "Please enter 2 numbers seperated by a space: ";
         cin >> num1 >> num2;
+
         cout << "\n";
+        // Adding the numbers//
         sum = add(num1, num2);
         cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
         cout << "\n";
+        // Subtracting the numbers//
         sum = subtract(num1, num2);
         cout << "The subtraction of " << num1 << " from " << num2 << " is " << sum << endl;
+
         cout << "\n";
+        // Multiplying the numbers//
+        sum = multiply(num1, num2);
+        cout << "The product of " << num1 << " and " << num2 << " is " << sum << endl;
+
+        // Dividing the numbers//
+        sum = divide(num1, num2);
+        cout << "The quotient of " << num1 << " and " << num2 << " is " << sum << endl;
+
+        cout << "\n";
+
+        // Loop to use a different set of numbers//
         cout << "Do you want to run another set of numbers?\n"
                 "Enter y for yes, or n for no: "
              << endl;
