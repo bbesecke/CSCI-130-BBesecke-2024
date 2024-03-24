4ad266cd11cd9513d1a823c37179337158198ea5
neo <bbesecke@mavs.coloradomesa.edu>
2024-03-23 19:06:02 -0600

    Spring Brake

--- Assignments/HW4/conditionals.cpp
+++ Assignments/HW4/conditionals.cpp
@@ -1,5 +1,53 @@
 /* Name: Burton Besecke 
 Class: CSCI 130-002
+Conditionals HW
+6. Write algorithm steps.
+
+7. Write and use a Makefile to build and run your program. (5 points)
+
+8. Use function prototypes. (5 points)
+
+9. Define a function that takes 5 numbers, calculates and returns the sum of the
+numbers. (5 points)
+
+10. Define a function that takes 5 numbers, calculates and returns the product of the
+numbers. (5 points)
+
+11. Define a function that takes 5 numbers, calculates and returns the average of the
+numbers. Must call the function you defined earlier to find the sum of the five
+numbers. (5 points)
+
+12. Define a function that takes 5 numbers, finds and returns the largest value
+among the numbers. Must use your own conditional statements and NOT use
+any built-in function. (5 points)
+
+13. Define a function that takes 5 numbers, finds and returns the smallest value
+among the numbers. Must use your own conditional statements and NOT use
+any built-in function. (10 points)
+
+14. Define a function that takes five numbers and returns if the floor of the sum of
+those numbers is even, odd or zero. (5 points)
+
+15. At least one function must be templated to work on various types (5 points).
+
+16. Greet the user by their name and prompt them to enter 5 numbers. (5 points)
+
+17. Display menu options for each function and call the specific function based on
+the user's choice to calculate the result using switch statement and display the
+result with proper descriptions. (5 points)
+
+18. Define a function called test (5*5=25 points)
+a. For each of the functions you defined above write at least 3 automated
+test cases using assert statements to automatically test and verify that the
+functions provide correct results.
+b. Try to come up with corner cases (various data) to confirm the functions
+still provide the correct results.
+
+19. Call the test function only if the “test” argument is passed to main. (5 points)
+
+20. 10 bonus points will be given if you make your program continue to run and
+calculate the statistical values of as many sets of 5 numbers as a user wishes
+until they want to quit the program.
 
 */
  
