b49fdfe225b9c064e4d89dbe3b408b2e6a6d115c
neo <bbesecke@mavs.coloradomesa.edu>
2024-03-30 18:57:16 -0600

    30 Mar

--- Labs/L5/main.cpp
+++ Labs/L5/main.cpp
@@ -1,6 +1,6 @@
 /*
     Loops Lab
-    Updated By:Burton Besecke   
+    Updated By:Burton Besecke
     CSCI 130
     Date: 30 Mar 2024
 
@@ -13,26 +13,25 @@
 
 using namespace std;
 
-
 void printTriangle(int height)
- {
-    //Function takes height as an argument to print the triangle
-    //of that height with *
+{
+    // Function takes height as an argument to print the triangle
+    // of that height with *
     int row = 1;
     // row
-    while (row <= height) {
+    while (row <= height)
+    {
         // column
-        for(int col = 1; col<=row; col++)
+        for (int col = 1; col <= row; col++)
             cout << "* ";
         row += 1;
         cout << endl;
     }
 }
 
-
-void printFlippedTriangle(int height) 
+void printFlippedTriangle(int height)
 {
-    /* 
+    /*
     Implement the function that takes height as an argument
     and prints a triangle with * of given height.
     Triangle of height 5, e.g., should look like the following.
@@ -41,67 +40,74 @@ void printFlippedTriangle(int height)
     * * *
     * *
     *
-    
+
     */
-    // FIXME3 ...
-}
+    int row = 1;
+    // row
+    while (row <= height)
+    {
+        // column
+        for (int col = 1; col <= row; col++)
+            cout << "* ";
+        row += 1;
+        cout << endl;
+        // FIXME3 ...
+    }
 
+    /*
+    FIXME4
+    Design and implement a function that takes an integer as height and
+    prints square of the given height with *.
+    Square of height 5, e.g., would look like the following.
 
-/*  
-FIXME4
-Design and implement a function that takes an integer as height and
-prints square of the given height with *.
-Square of height 5, e.g., would look like the following.
+    *  *  *  *  *
+    *  *  *  *  *
+    *  *  *  *  *
+    *  *  *  *  *
+    *  *  *  *  *
 
-*  *  *  *  *  
-*  *  *  *  *   
-*  *  *  *  *   
-*  *  *  *  *   
-*  *  *  *  *   
+    */
 
-*/
-
-// function clears the screen system call
-// NOTE: system call is not a security best pracice!
-void clearScreen() 
-{
-    // use "cls" in windows
-    #ifdef _WIN32
+    // function clears the screen system call
+    // NOTE: system call is not a security best pracice!
+    void clearScreen()
+    {
+// use "cls" in windows
+#ifdef _WIN32
         system("clS");
-    #endif
-   
-}
+#endif
+    }
 
-int main(int argc, char* argv[]) 
+    int main(int argc, char *argv[])
 
- {
-    bool keepRunning = true;
-     while (keepRunning)
-    // FIXME5 add a loop to make the program to continue to run until the user wants to quit
-    // FIXME6 call clearScreen function to clear the screen for each round of the loop
-    int height;
-    cout << "Program prints geometric shapes of given height with *\n";
-    cout << "Please enter the height of the shape: ";
-    cin >> height;
-    // call printTriangle function passing user entered height
-    printTriangle(height);
+    {
+        bool keepRunning = true;
+        while (keepRunning)
+            // FIXME5 add a loop to make the program to continue to run until the user wants to quit
+            // FIXME6 call clearScreen function to clear the screen for each round of the loop
+            int height;
+        cout << "Program prints geometric shapes of given height with *\n";
+        cout << "Please enter the height of the shape: ";
+        cin >> height;
+        // call printTriangle function passing user entered height
+        printTriangle(height);
 
-    // FIXME7
-    // Call printFlippedTriangle passing proper argument
-    // Manually test the function
+        // FIXME7
+        // Call printFlippedTriangle passing proper argument
+        // Manually test the function
 
-    // FIXME6
-    // Call the function defined in FIXME4 passing proper argument
-    // Manually test the function
+        // FIXME6
+        // Call the function defined in FIXME4 passing proper argument
+        // Manually test the function
 
-    // FIXME9
-    // prompt user to enter y/Y to continue anything else to quit
+        // FIXME9
+        // prompt user to enter y/Y to continue anything else to quit
 
-    // FIXME10
-    // Use conditional statements to break the loop or continue the loop
+        // FIXME10
+        // Use conditional statements to break the loop or continue the loop
 
-{
-    keepRunning = false;
-}
-    return 0;
-}
+        {
+            keepRunning = false;
+        }
+        return 0;
+    }
