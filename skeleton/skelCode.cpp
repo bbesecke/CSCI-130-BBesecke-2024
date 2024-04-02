6bd88624dc41a2857b1b42867951200608a82e9e
neo <bbesecke@mavs.coloradomesa.edu>
2024-04-02 09:14:46 -0600

    02 Apr

---
+++ skeleton/skelCode.cpp
@@ -0,0 +1,21 @@
+/* Name: Burton Besecke
+Class: CSCI 130-002
+
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+   for (int hgt = 1; hgt <= col; hgt++)
+            cout << "* ";
+        col += 1;
+        cout << endl;
+
+   return 0;
+}
