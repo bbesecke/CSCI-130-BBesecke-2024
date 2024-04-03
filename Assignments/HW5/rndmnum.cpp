dfbcbf52c1eb34dee823ac414b1f3caea51a4fcf
neo <bbesecke@mavs.coloradomesa.edu>
2024-04-03 09:15:19 -0600

    03 Apr

---
+++ Assignments/HW5/rndmnum.cpp
@@ -0,0 +1,19 @@
+/* Name: Burton Besecke
+Class: CSCI 130-002
+
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+
+{
+ 
+ cout << "Yellow World!!" << endl;
+ 
+   return 0;
+}
