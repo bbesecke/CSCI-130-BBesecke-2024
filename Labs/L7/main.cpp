6a306edc9788706bc04721b0ccf02622954bb1e4
neo <bbesecke@mavs.coloradomesa.edu>
2024-04-16 09:25:34 -0600

    16 Apr 0925

--- Labs/L7/main.cpp
+++ Labs/L7/main.cpp
@@ -1,8 +1,8 @@
 /*
 Kattis - Hissing Microphone Problem
 
-By: FIXME1
-Date: FIXME2
+By: Burton Besecke FIXED
+Date: 15 Apr
 
 Hissing Microphone Problem Statement: https://open.kattis.com/problems/hissingmicrophone
 Algorithm steps:
@@ -23,33 +23,45 @@ string answer(const string &line);
 void testAnswer();
 void solve();
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     if (argc == 2 and string(argv[1]) == "test")
         testAnswer();
     else
         solve();
 }
 
-string answer(const string &line) {
-    // FIXME3
+string answer(const string &line)
+{
+    // FIXME3 FIXED
     // implment algorithm step 2
     // return "hiss" if ss is found in line
     // otherwise, return "no hiss"
-    return "hiss";
+    if (line.find("ss") != string::npos)
+        return "Hiss";
+    else
+        return "No hiss";
 }
 
 // unit testing answer()
-void testAnswer() {
-    // FIXME4
+void testAnswer()
+{
+    // FIXME4 FIXED
     // write at least two test cases to test answer()
+    // 1 test
+    assert(answer("snake") == "no hiss");
+    assert(answer("hiss") == "hiss");
+    // 2 test
     cerr << "All test cases passed!\n";
 }
 
 // solving the problem for kattis
-void solve() {
+void solve()
+{
     string line;
     // string consists of only lowercase letters (no spaces) upto 30 chars
-    // FIXME5
+    // FIXME5 FIXED
     // read string into line
+    cin >> line;
     cout << answer(line) << endl;
 }
