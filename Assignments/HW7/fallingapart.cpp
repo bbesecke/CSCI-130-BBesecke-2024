76abe779e46819fd99aa52d5ceaa67e08855f97f
neo <bbesecke@mavs.coloradomesa.edu>
2024-04-17 14:14:25 -0600

    17 Apr 1415 NF

---
+++ Assignments/HW7/fallingapart.cpp
@@ -0,0 +1,22 @@
+/* Name: Burton Besecke
+Class: CSCI 130-002
+
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2 && string(argv[1]) == "test")
+    {
+        test();
+    }
+    else
+    {
+    }
+    return 0;
+}
