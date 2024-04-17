82de7bcf32443add948151bf224d702b5d2d2712
neo <bbesecke@mavs.coloradomesa.edu>
2024-04-17 13:22:03 -0600

    17 Apr 1330

--- Assignments/HW6/string.cpp
+++ Assignments/HW6/string.cpp
@@ -1,52 +1,52 @@
 // By: Burton Besecke
 // Date: 16 Apr
-
-// Apaxxians on Kattis
-/* With all lowercase letters, take a name with multiple instances of recusrsive letters and
- make the recursive letter into one instance*/
-
-//  Write programmer info and briefly describe the program as comments.
-
-//  Write algorithm steps as comments (10 points).
-
-//  Using function(s) design your solution in such a way that it can be tested independently
-// using unit testing with assert statements (40 points).
-
-//  Write a test function that automatically tests (using assert statements) for at least three
-// different input values other than the ones provided by the problem (20 points).
-
-//  Call the test function in main and execute it when a “test” argument is provided to your
-// program or main(). (10 points)
-
-//  Create and use a Makefile to test kattis sample input files as well as your unit testing (10 points).
-
-//  Once the solution is well tested locally (create screenshots) upload to kattis online
-// judge for judging. You can submit your solution as many times as you wish but your goal
-// is to get an Accepted verdict in as few tries as possible.
-
-//  Once the solution is accepted, create a screenshot that shows your full name and the
-
-// Accepted verdict for the problem and submit it to D2L.
+// Make program that follows the Kattis Apaxian thingy
 
 #include <iostream>
 #include <cassert>
 #include <string>
 
 using namespace std;
-string shorter(string &);
+string shorter(string &, string &);
 void test();
 
-int main()
+int main(int argc, char *argv[])
 {
-    string name;
-    string short;
-    shorter(name, short);
-    cout << " Enter the name: " << endl;
-    cin >> name >> endl;
-    cout << shorter << " is the easier version." << endl;
-
-    return 0; // This exits the program, huzzah!
+    if (argc == 2 && string(argv[1]) == "test")
+    {
+        test();
+    }
+    else
+    {
+        // Enter a name with repeating letters to get a version with only a single instance of the repeating letters
+        // Variables for full Apaxian name and shortened version
+        string name;
+        string shart;
+        cout << " Enter the name: " << endl;
+        cin >> name;
+        cout << shorter(name, shart) << " is the shortened version." << endl;
+        cout << shorter(name, shart);
+        return 0;
+    }
 }
-string shorter(string &name, string &short)
+// Function for canceling repeating letters after first instance
+string shorter(string &name, string &shart)
 {
+    shart = name[0];
+    for (size_t i = 1; i < name.size(); i++)
+    {
+        if (name[i] != name[i - 1])
+            shart += name[i];
+    }
+    return shart;
+}
+// 3 tests for shorter function
+void test()
+{
+    string test1 = "Boob", test2 = "Norm", test3 = "Staaaan", shart;
+    assert(shorter(test1, shart) == "Bob");
+    assert(shorter(test2, shart) == "Norm");
+    assert(shorter(test3, shart) == "Stan");
+
+    cout << "Tests worked." << endl;
 }
