7868000215999d6d86b2a313638ab955f4bbcad7
neo <bbesecke@mavs.coloradomesa.edu>
2024-04-25 08:59:40 -0600

    25 Apr

--- skeleton/skelCode.cpp
+++ skeleton/skelCode.cpp
@@ -2,16 +2,23 @@
 
 #include <iostream>
 
-
 using namespace std;
-
 
 int main(int argc, char *argv[])
 {
-for (size_t i = 0; i < 20; i++)
-{
-    cout << "Hipp, Hipp"<< endl;
-}
+  int rpt;
+  string word;
+  cin >> word;
+  cin >> rpt;
 
-   return 0;
+  if (rpt <= 0)
+  {
+    cout << "Repeat count should be greater than 0." << endl;
+    return 1;
+  }
+
+  for (int i = 0; i < rpt; i++)
+  {
+    cout << word;
+  }
 }
