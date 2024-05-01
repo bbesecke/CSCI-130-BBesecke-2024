ade8c16e960541eef7d99c739da88db0d526ef28
neo <bbesecke@mavs.coloradomesa.edu>
2024-05-01 09:17:19 -0600

    01 May

--- ZKattis /main.cpp
+++ ZKattis /main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
 using namespace std;
-int parked(int num1, int num2, int num3);
 
-int main(int argc, char *argv[])
+int parked(int, int);
+
+int main()
 {
-    int num1, num2, num3;
-    parked(num1, num2, num3); 
-}
-int parked(int num1, int num2, int num3)
-{
-    
+    int num1, num2;
     cin >> num1;
     cin >> num2;
-    num3 = num1 + num2;
+    int num3 = parked(num1, num2);
     cout << num3 << endl;
-
     return 0;
+}
+
+int parked(int num1, int num2)
+{
+    return num1 + num2; 
 }
